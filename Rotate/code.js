b2633c63b4acba4b57ccfefdaf98d1ab112a2142
Dhruvik Gondaliya <yashgondaliya04@gmail.com>
2023-10-06 15:12:19 +0400

    Solve the rotate, problem no. 48

--- Rotate/code.js
+++ Rotate/code.js
@@ -12,29 +12,60 @@ Output: [[7,4,1],[8,5,2],[9,6,3]] */
  */
 
 
-var rotate = function (matrix) {
+var rotate = function (arr) {
 
-    let k = matrix.length - 1;
+    /*     let k = matrix.length - 1;
+    
+        let arr = [...matrix];
+    
+        console.log(arr);
+    
+        for (let i = 0; i < matrix.length; i++) {
+    
+            for (let j = 0; j < matrix[i].length; j++) {
+    
+                     arr[j][k] = matrix[i][j];
+    
+                    // console.log(matrix[j][i]);
+            
+            }
+    
+            k--;
+        }
+        console.log(arr); */
 
-    for (let i = 0; i < matrix.length; i++) {
 
-        for (let j = 0; j < matrix.length; j++) {
+    let x = 0;
+    let y = arr.length - 1;
 
+    while (x < y) {
+
+        for (let i = 0; i < y - x; i++) {
+
+            let temp = arr[x][x + i];
+            arr[x][x + i] = arr[y - i][x];
+            arr[y - i][x] = arr[y][y - i];
+            arr[y][y - i] = arr[x + i][y];
+            arr[x + i][y] = temp;
 
         }
+        x++;
 
-        k--;
+        y--;
     }
-    console.log(matrix);
+
+
+    console.log(arr);
 };
 
 
 
 const arr =
     [
-        [1, 2, 3],
-        [4, 5, 6],
-        [7, 8, 9]
+        [1, 2, 3, 20],
+        [4, 5, 6, 21],
+        [7, 8, 9, 22],
+        [10, 11, 12, 23]
     ];
 
-console.log(rotate(arr));
+(rotate(arr));
