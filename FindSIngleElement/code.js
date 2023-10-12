6159f4299495e51efacb2cfc716b85e031505eb9
Dhruvik Gondaliya <yashgondaliya04@gmail.com>
2023-10-12 15:17:51 +0400

    Solve the FindSingle, problem no. 35

--- FindSIngleElement/code.js
+++ FindSIngleElement/code.js
@@ -10,6 +10,7 @@ Example 1:
 
 Input: nums = [1,1,2,3,3,4,4,8,8]
 Output: 2
+
 Example 2:
 
 Input: nums = [3,3,7,7,10,11,11]
@@ -22,9 +23,29 @@ Output: 10 */
 
 var singleNonDuplicate = function (nums) {
 
-    
+    let start = 0;
+    let end = nums.length - 1;
+
+    while (start < end) {
+
+        let mid = Math.floor((start + end) / 2) + 1;
+
+        if (mid % 2 == 1) {
+            mid--;
+        }
+
+        if (nums[mid] != nums[mid + 1]) {
+            end = mid;
+        }
+        else {
+            start = mid + 2;
+        }
+
+    }
+    return nums[start];
+
 };
 
-const nums = [3,3,7,7,10,11,11]
+const nums = [3, 3, 7, 7, 10, 11, 11]
 
 console.log(singleNonDuplicate(nums));
