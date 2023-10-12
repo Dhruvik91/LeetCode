88880c3c5781d335223091f6bf6cc5f6248577e1
Dhruvik Gondaliya <yashgondaliya04@gmail.com>
2023-10-12 12:10:13 +0400

    Solve the Insert ELement, problem no. 35

--- InsertElement/code.js
+++ InsertElement/code.js
@@ -18,3 +18,43 @@ Input: nums = [1,3,5,6], target = 7
 Output: 4 */
 
 
+function binarySearch(arr, target) {
+
+    let start = 0;
+    let end = arr.length - 1;
+
+    while (start <= end) {
+
+        let mid = Math.floor((start + end) / 2)
+
+        if (arr[mid] < target) {
+            start = mid + 1; 
+        }else {
+            end = mid - 1
+        }
+
+    }
+    return start;
+
+}
+
+const array = [1,3,5,6];
+
+
+console.log(binarySearch(array, 2));
+
+/* function addElement(array, target) {
+
+    if (!binarySearch(array,target)) {
+
+        array.push(target);
+        array.sort((a,b) => a - b);
+    }
+
+    let indexValue = binarySearch(array,target)
+
+    return (indexValue);
+
+}
+
+console.log(addElement(array,2)); */
