d0af9af06ebaf17024a41982c05adeb6c9ba22de
Dhruvik Gondaliya <yashgondaliya04@gmail.com>
2023-10-13 11:36:27 +0400

    Solve Intersection, problem no. 349

---
+++ Intersection/code.js
@@ -0,0 +1,45 @@
+/* Problem no. 349 */
+
+/* Given two integer arrays nums1 and nums2, return an array of their intersection. Each element in the result must be unique and you may return the result in any order.
+
+ 
+
+Example 1:
+
+Input: nums1 = [1,2,2,1], nums2 = [2,2]
+Output: [2]
+Example 2:
+
+Input: nums1 = [4,9,5], nums2 = [9,4,9,8,4]
+Output: [9,4]
+Explanation: [4,9] is also accepted. */
+
+
+/**
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @return {number[]}
+ */
+
+var intersection = function (nums1, nums2) {
+
+    let set1 = new Set(nums1);
+    let set2 = new Set(nums2);
+    let result = [];
+
+    for (let values of set1.values()) {
+
+        if (set2.has(values)) {
+            result.push(values)
+        }
+    }
+
+    console.log(result);
+};
+
+
+let nums1 = [2];
+let nums2 = [2, 2, 2, 2, 5, 6, 7, 8]
+
+intersection(nums1, nums2);
+
